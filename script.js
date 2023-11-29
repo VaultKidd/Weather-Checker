38844528c34bf3fcfe75b5f4943bfe1753e9e386
Igor <igorkozakiewicz13@gmail.com>
2023-11-29 18:28:35 +0000

    added promise to use API info for weather, search button not working, need to fix

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const apiKey = "516e2e494eba1de1363529fbb76f3445";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=olsztyn";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather () {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
-    const data = await response.json()
+const searchBox = document.querySelector('.search__bar input');
+const searchBtn = document.querySelector('.search__bar button');
+
+
+async function checkWeather (city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json()
     console.log(data)
+    
+    document.querySelector('.city').innerHTML = data.name + ', ' + data.sys.country;
+    document.querySelector('.temp').innerHTML = (Math.round(data.main.temp * 10) / 10) + ' °C';
+    document.querySelector('.humidity__percent').innerHTML = data.main.humidity + '%';
+    document.querySelector('.wind__speed').innerHTML = data.wind.speed + 'km/h';
+    
 }
-checkWeather();
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
