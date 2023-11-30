cf389275b748281af890cd52db48754e7b034f1e
Igor <igorkozakiewicz13@gmail.com>
2023-11-30 19:30:05 +0000

    added functionality to app, search for any city now works, and information is not displayed until searched for, need to add list of locations as well as autocomplete and a failure to search state

--- script.js
+++ script.js
@@ -3,20 +3,46 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector('.search__bar input');
 const searchBtn = document.querySelector('.search__bar button');
-
+const weatherIcon = document.querySelector('.temp__icon');
+const searchBar = document.querySelector('.search__bar');
 
 async function checkWeather (city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json()
     console.log(data)
     
+    document.getElementById('weather').style.opacity = '1';
+    document.getElementById('weather').style.transition = 'all 700ms ease';
+
     document.querySelector('.city').innerHTML = data.name + ', ' + data.sys.country;
     document.querySelector('.temp').innerHTML = (Math.round(data.main.temp * 10) / 10) + ' °C';
     document.querySelector('.humidity__percent').innerHTML = data.main.humidity + '%';
     document.querySelector('.wind__speed').innerHTML = data.wind.speed + 'km/h';
     
+    if(data.weather[0].main == 'Clouds') {
+        weatherIcon.src = "./assets/overcast.png";
+    }
+    else if(data.weather[0].main == 'Clear'){
+        weatherIcon.src = './assets/sunny.png';
+    }
+    else if(data.weather[0].main == 'Rain'){
+        weatherIcon.src = './assets/rain.png';
+    }
+    else if(data.weather[0].main == 'Snow'){
+        weatherIcon.src = './assets/snowy.png';
+    }
+    else if(data.weather[0].main == 'Mist'){
+        weatherIcon.src = './assets/mist.png';
+    }
+    else if(data.weather[0].main == 'Thunderstorm'){
+        weatherIcon.src = './assets/heavy-rain.png';
+    }
 }
 
-searchBtn.addEventListener("click", ()=>{
+searchBtn.addEventListener('click', ()=>{
     checkWeather(searchBox.value);
+})
+
+searchBar.addEventListener('click', ()=>{
+    document.getElementById('search__bar').style.top = '24px';
 })
